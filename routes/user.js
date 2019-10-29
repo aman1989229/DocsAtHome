88790f20223502d82815c8feeb4ea92cb17cea0a
Aman <amankumarshrivas@gmail.com>
2019-10-29 23:54:46 +0530

    signup page setup with backend data is sending into the database

--- routes/user.js
+++ routes/user.js
@@ -4,11 +4,7 @@ var User= require('../models/user');
 router.get('/signup',(req,res)=>{
     res.render('signup');
 });
- router.post('/signup',(req,res,next)=>{
-   var user= new User();
-   user.name=req.body.name;
-   user.email=req.body.email;
- });
+
 
 
  module.exports=router;
