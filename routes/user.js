30e41893467f6ee9d76aeff4e70b0574f008d9a4
Aman <amankumarshrivas@gmail.com>
2019-10-30 01:50:28 +0530

    validation is ready to use for check password length email exists or not fields are not blank etc

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,52 @@ var router = require('express').Router();
 var User= require('../models/user');
 
 router.get('/signup',(req,res)=>{
-    res.render('signup');
-});
+  res.render('signup');
+  });
+ 
+  router.get('/signin',(req,res)=>{
+   res.render('signin');
+   });
+
+   router.post('/signup',(req,res)=>{
+    
+     const{name,email,phone,password}=req.body;
+     let errors =[];
+       if(!name||!email||!phone||!password){
+         errors.push({msg:'please fill in all fields!!!'});
+       }
+       if(password.length<6){
+        errors.push({msg:'password should be at least 6 characters long!!!'});
+       }
+       if(errors.length>0){
+             res.render('signup',{
+               errors,name,email,phone,password
+              });
+       }
+       else{
+          User.findOne({email:email})
+          .then(user=>{
+            if(user){
+              errors.push({msg:'email already exist!!!'});
+              res.render('signup',{
+                errors,name,email,phone,password
+               });
+            }
+            else{
+              let newuser =new User({
+                name,
+                email,
+                phone,
+                password
+               });
+               newuser.save();
+               console.log('success');
+             }
+          });
+        }
+      });
+
+         
 
 
 
